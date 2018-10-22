8d1d77f85d491a63123189b861964336af38e44d
Alex <alnovoselov@mail.ru>
2018-10-22 20:16:08 +0300

    Full test coverage

--- src/test/java/org/github/qweek/CalculatorTest.java
+++ src/test/java/org/github/qweek/CalculatorTest.java
@@ -47,6 +47,7 @@ class CalculatorTest {
     @ParameterizedTest(name = "{index}: {arguments}.add(...)")
     @MethodSource("calculators")
     void testAdd(Calculator calculator) {
+        assertEquals(0, calculator.add(null));
         assertEquals(0, calculator.add(""));
         assertEquals(1, calculator.add("1"));
         assertEquals(3, calculator.add("1,2"));
